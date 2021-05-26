82769e66e1971020b1037e68cb08c9bd77131e74
Henrique <riquessan@gmail.com>
2021-05-26 15:34:56 -0300

    Port Changes

--- config/config.js
+++ config/config.js
@@ -2,8 +2,6 @@ var express = require("express");
 var logger = require("morgan");
 const modules = "../src/modules";
 const routePath = require("./routes_paths");
-// const indexRouter = require(`${modules}/authentication/routes/authentication`);
-// const setRoute = require("./set_route");
 
 var app = express();
 require("./middleware")(app, logger, express);
